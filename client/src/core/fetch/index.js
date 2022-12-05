d09c628ad4a5de115a764c0d2e21332b5261ea1f
Tomáš Kubíček <tomas.kubicek@morosystems.cz>
2022-12-05 19:01:03 +0100

    Import fix

--- client/src/core/fetch/index.js
+++ client/src/core/fetch/index.js
@@ -1,7 +1,7 @@
 import RequestError from "./RequestError";
 import UnauthorizedError from "./UnauthorizedError";
 import coreApi from "./coreApi";
-import {doDelete, doGet, doGetExternal, doGetPlain, doPost, doPut, PRECOGNITION_FAILED, timeoutPromise,} from "./fetch";
+import {doDelete, doGet, doGetExternal, doGetPlain, doPost, doPut, PRECOGNITION_FAILED, timeoutPromise} from "./fetch";
 
 export default {
     PRECOGNITION_FAILED,
