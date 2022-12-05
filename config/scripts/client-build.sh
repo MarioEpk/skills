7269cc0bcb5ca361d75df3e75d3888246c4395a7
Michal Šípek <michal.sipek@morosystems.cz>
2022-12-05 12:28:57 +0000

    MCV-46 Fix missing required parameter "client_id"

--- config/scripts/client-build.sh
+++ config/scripts/client-build.sh
@@ -2,12 +2,11 @@
 
 echo "Compiling the frontend code..."
 
+cd client
 [ -e .env ] && rm .env
 echo "REACT_APP_GOOGLE_CLIENT_ID=${GOOGLE_CLIENT_ID}" >> .env
 echo "REACT_APP_API_URL=${CLIENT_API_URL}" >> .env
 
 apt install -y git
-cd client
 npm install
 npm run build:ui
-
